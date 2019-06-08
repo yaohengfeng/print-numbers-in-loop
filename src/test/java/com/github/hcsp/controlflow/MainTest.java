42af6d0932575098e91b607239ca5028a3c8dc76
Bo Zhang <bo@gradle.com>
2019-06-08 22:59:28 +0800

    Update to latest state

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,22 +1,23 @@
 package com.github.hcsp.controlflow;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import com.github.blindpirate.extensions.CaptureSystemOutput;
-import org.junit.jupiter.api.Test;
-import java.util.Random;
-import java.util.stream.IntStream;
+
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
 
+import com.github.blindpirate.extensions.CaptureSystemOutput;
+import java.util.Random;
+import java.util.stream.*;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
-  @Test
-  @CaptureSystemOutput
-  public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
-    int n = new Random().nextInt();
-    capture.expect(containsStringIgnoringCase(
-      IntStream.range(-n, n+1).filter(i -> i % 2 != 0).mapToObj(Integer::toString).collect(Collectors.joining(","))
-    );
-    Main.printOddNumbersBetween(-n, n);
-  }
+    @Test
+    @CaptureSystemOutput
+    public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
+        int n = new Random().nextInt(100);
+        capture.expect(
+                containsStringIgnoringCase(
+                        IntStream.range(-n, n + 1)
+                                .filter(i -> i % 2 != 0)
+                                .mapToObj(Integer::toString)
+                                .collect(Collectors.joining(","))));
+        Main.printOddNumbersBetween(-n, n);
+    }
 }
