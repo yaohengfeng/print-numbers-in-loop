daf0d6c7bec0838d9ef5be47207710febf79464f
Rokixy <48944401+Rokixy@users.noreply.github.com>
2019-06-22 22:40:58 +0800

    print odd numbers (#15)
    print odd numbers (#15)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,16 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        for (int i = start; i <end+1 ; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+
+                if (i < end || i < end - 1) System.out.print(",");
+
+            }
+        }
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
