a9b7ce4d519476dfde20e87c6fa8f67cc524a698
Zhang Xin <zhangxindev@gmail.com>
2019-07-11 21:35:49 +0800

    逗号比较难解决

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,10 +9,21 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        for (int i = start; i <= end; i ++) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+
+                if (end - i >= 2) {
+                    System.out.print(",");
+                }
+            }
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        printOddNumbersBetween(1, 5);
+        printOddNumbersBetween(1, 2);
         printOddNumbersBetween(-2, 2);
     }
 }
