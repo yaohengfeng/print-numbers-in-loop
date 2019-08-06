68d008580862f407d1360e3a40830c45b7533d16
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2019-08-06 21:28:01 +0800

    循环 (#45)
    循环 (#45)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -2,14 +2,26 @@ package com.github.hcsp.controlflow;
 
 public class Main {
     /**
-     * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。 注意，数字之间用英文逗号分隔。
+     * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。
+     * 注意，数字之间用英文逗号分隔。
      *
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        String string = "";
+        do {
+            if (start%2==0){
+                start++;
+                continue;
+            }
+            string= string==""?(string+""+start):(string+","+start);
+            start++;
+        } while (start<end||start==end);
+        System.out.println(string);
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
