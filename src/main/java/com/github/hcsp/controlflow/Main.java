d3641cf82d1fc1ce91ce755dc6f5e35e7ddc6561
wpsumsun <wpsumsun@126.com>
2019-09-19 17:47:35 +0800

    print numbers in loop (#65)
    print numbers in loop (#65)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,9 +7,21 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        String s = "";
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                if (s.length() == 0) {
+                    s += i;
+                } else {
+                    s += ("," + i);
+                }
+            }
+        }
+        System.out.println(s);
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
