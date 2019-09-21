f96bd73479fe94c13bc1bdb0a5abb4011b44dfb1
Nicklzy <tydx11@hotmail.com>
2019-09-21 22:09:49 +0800

    打印奇数 (#67)
    打印奇数 (#67)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,9 +7,19 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        StringBuilder str = new StringBuilder();
+        for (int i = start; i < (end - start + 1); i++) {
+            if (Math.abs(i) % 2 != 0) {
+                str.append(i).append(",");
+            }
+        }
+        if (!str.toString().equals("")) {
+            System.out.println(str.substring(0, str.length() - 1));
+        }
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
