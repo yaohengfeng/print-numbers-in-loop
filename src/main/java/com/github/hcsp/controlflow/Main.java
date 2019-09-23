ced491bc5fbbe8b4d48c29ebc08b7296989ea1d1
Hank <hankhu163@foxmail.com>
2019-09-23 13:50:25 +0800

    正确处理好 , (#69)
    正确处理好 , (#69)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,9 +7,25 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        // 打印最后一个奇数，后面不加 ","
+        // 即 end 为奇数，单独判断打印后 1 位
+        // end 为偶数，单独判断打印后 2 位
+        // 注意 start end 有负数情况
+        int tempEnd = end % 2 == 0 ? end - 2 : end - 1;
+        for (int i = start; i <= tempEnd; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + ",");
+            }
+        }
+        for (int j = tempEnd; j <= end; j++) {
+            if (j % 2 != 0) {
+                System.out.println(j);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
