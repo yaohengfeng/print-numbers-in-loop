3f9c46b39f1baa9de171fe9f91fb95cc06d8f5b2
dqprecisian <1352034864@qq.com>
2019-10-16 18:44:39 +0800

    print numbers in loop with specific condition.

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,9 +12,14 @@ public class Main {
     public static void printOddNumbersBetween(int start, int end) {
         for (int i = start; i <= end; i++) {
             if (i % 2 != 0) {
-                System.out.println(i);
+                if(i == end || i == end - 1) {
+                    System.out.print(i);
+                    break;
+                }
+                System.out.print(i + ",");
             }
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
