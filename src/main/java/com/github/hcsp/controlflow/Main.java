f6b5bd350eaa74c9a4decdf8cd51e5655a264bb7
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2019-11-03 15:31:43 +0800

    1 (#88)
    1 (#88)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,20 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        boolean flag =false;
+        for(int i=start;i<=end;i++){
+            if(i%2!=0) {
+                if (!flag) {
+                    System.out.print(i);
+                    flag = true;
+                }
+                else{
+                    System.out.print(","+i );
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
