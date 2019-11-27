bd3d7afc098917168c2963d5d2cc6db57ea3cd88
wgd <872440899@qq.com>
2019-11-27 13:04:01 +0800

    实现join方法 (#93)
    实现join方法 (#93)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     /**
      * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。 注意，数字之间用英文逗号分隔。
@@ -9,7 +12,15 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=start; i<=end; i++){
+           if(i%2!=0){
+               list.add(i);
+           }
+        }
+        System.out.println(list.toString().replace("[", "").replace("]", "").replace(" ", ""));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
