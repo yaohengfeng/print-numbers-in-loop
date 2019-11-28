9481ab44ce3ecc08236a4d6d09c3389da82097e1
Ryan <49991585+Ryan-Y-C@users.noreply.github.com>
2019-11-28 09:23:59 +0800

    循环打印指定的数字。 (#95)
    循环打印指定的数字。 (#95)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,9 +7,17 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        StringBuffer temp = new StringBuffer();
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                temp.append(i + ",");
+            }
+        }
+        System.out.println(temp.substring(0, temp.length() - 1));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
