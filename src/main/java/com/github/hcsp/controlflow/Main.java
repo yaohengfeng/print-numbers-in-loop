faaae26c495addfbd6dc5f7450357d9ebeefb1e7
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-02 13:52:51 +0800

    feat: 打印所有奇数 (#100)
    feat: 打印所有奇数 (#100)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,13 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        String s = "";
+        for(int i = start; i < end; i++) {
+            if (i % 2 != 0) s = s + i + ',';
+        }
+        System.out.println(s.substring(0, s.length() -1));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
